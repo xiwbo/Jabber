717544321a6493a18f4a874f745d0564148be3f6
Queen <ArlynQueen10>
2018-08-23 18:53:48 +0800

    Added: About Screen and Forgot Password Screen with functionality

--- src/main/java/com/jabber/RegisterScreen.java
+++ src/main/java/com/jabber/RegisterScreen.java
@@ -1,28 +1,34 @@
 package com.jabber;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
 import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.firebase.client.Firebase;
 
-public class RegisterScreen extends AppCompatActivity {
-
+public class RegisterScreen extends AppCompatActivity
+{
 	private FirebaseAuth mAuth;
 	Button registerbtn;
 	TextView loginLink;
-	EditText email;
-	EditText password;
+	EditText email,username,password,confirmPassword;
+	CheckBox tickbox;
 	String user,pass;
 	Firebase firebase;
 
@@ -34,8 +40,17 @@ public class RegisterScreen extends AppCompatActivity {
 		firebase = new Firebase("https://jabber-6ac14.firebaseio.com");
 		mAuth = FirebaseAuth.getInstance();
 		registerbtn = findViewById(R.id.btnRegister);
-		email = findViewById(R.id.navEmail);
+		email = findViewById(R.id.txtFEmail);
+		username = findViewById(R.id.txtUsername);
 		password = findViewById(R.id.txtPassword);
+		confirmPassword = findViewById(R.id.txtConfirmPw);
+		tickbox = findViewById(R.id.tickbox);
+		registerbtn.setEnabled(false);
+		email.addTextChangedListener(textWatcher);
+		username.addTextChangedListener(textWatcher);
+		password.addTextChangedListener(textWatcher);
+		confirmPassword.addTextChangedListener(textWatcher);
+		confirmPassword.setOnEditorActionListener(editorActionListener);
 		loginLink = findViewById(R.id.linkLogin);
 		loginLink.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -48,9 +63,15 @@ public class RegisterScreen extends AppCompatActivity {
 		registerbtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				user = email.getText().toString();
-				pass = password.getText().toString();
-				createAccount();
+				if(!email.getText().toString().contains("@") || !email.getText().toString().contains(".com")) {
+					Toast.makeText(getApplicationContext(), "Please check your email address.", Toast.LENGTH_SHORT).show();
+				}
+				else if(password.length() < 6) {
+					Toast.makeText(getApplicationContext(), "Please check password, make sure your password is atleast 6 characters.", Toast.LENGTH_SHORT).show();
+				}
+				else {
+					OnClick();
+				}
 			}
 		});
 	}
@@ -72,4 +93,42 @@ public class RegisterScreen extends AppCompatActivity {
 			}
 		});
 	}
+
+	public void OnClick() {
+		if(password.getText().toString().equals(confirmPassword.getText().toString())) {
+			user = email.getText().toString();
+			pass = password.getText().toString();
+			createAccount();
+		}
+		else {
+			Toast.makeText(getApplicationContext(), "Password does not match.", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private TextWatcher textWatcher = new TextWatcher() {
+		@Override
+		public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+		}
+		@Override
+		public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+			registerbtn.setEnabled(!email.getText().toString().trim().isEmpty() && !username.getText().toString().trim().isEmpty() && !password.getText().toString().trim().isEmpty() && !confirmPassword.getText().toString().trim().isEmpty() && tickbox.isChecked());
+		}
+		@Override
+		public void afterTextChanged(Editable editable) {
+		}
+	};
+
+	private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+		@Override
+		public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+			switch(i) {
+				case EditorInfo.IME_ACTION_SEND:
+					if(!email.getText().toString().trim().isEmpty() && !username.getText().toString().trim().isEmpty() && !password.getText().toString().trim().isEmpty() && !confirmPassword.getText().toString().trim().isEmpty() && tickbox.isChecked()) {
+						OnClick();
+					}
+				break;
+			}
+			return(false);
+		}
+	};
 }
